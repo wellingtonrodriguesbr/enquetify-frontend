d54233ee43706b62f00b59c2b2e51958d6970ce1
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-04-12 05:38:56 -0300

    update

--- src/app/autenticacao/page.tsx
+++ src/app/autenticacao/page.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import { LoadingLottie } from "@/components/loading";
-import { useValidatedCode } from "@/hooks/use-validated-code";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Authenticate() {
-  const params = useSearchParams();
-
-  useValidatedCode({
-    code: params.get("codigo"),
-  });
-
   return (
     <section className="flex justify-center items-center h-screen w-full bg-white">
-      <LoadingLottie />
+      <Suspense>
+        <LoadingLottie />
+      </Suspense>
     </section>
   );
 }
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,8 +2,16 @@
 
 import Lottie from "lottie-react";
 import loading from "@/lib/lotties/loading.json";
+import { useSearchParams } from "next/navigation";
+import { useValidatedCode } from "@/hooks/use-validated-code";
 
 export function LoadingLottie() {
+  const params = useSearchParams();
+
+  useValidatedCode({
+    code: params.get("codigo"),
+  });
+
   return (
     <div className="w-[300px] h-[300px]">
       <Lottie animationData={loading} loop={true} />
