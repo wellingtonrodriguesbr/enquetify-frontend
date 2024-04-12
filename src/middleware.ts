19d1c50c6511cd940433a444e4d29724cf0ee651
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-04-12 05:53:05 -0300

    chore: remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-  if (!request.cookies.get("refreshToken")?.value) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-}
+// // This function can be marked `async` if using `await` inside
+// export async function middleware(request: NextRequest) {
+//   if (!request.cookies.get("refreshToken")?.value) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+// }
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ["/enquetes", "/enquete/:path*"],
-};
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: ["/enquetes", "/enquete/:path*"],
+// };
